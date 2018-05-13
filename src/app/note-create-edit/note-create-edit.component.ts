3c744af65e05425b89bc7257f93f054671c09ee6
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-05-13 15:54:29 +0200

    Get Note Data

--- src/app/note-create-edit/note-create-edit.component.ts
+++ src/app/note-create-edit/note-create-edit.component.ts
@@ -1,4 +1,7 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NoteService } from '../services/notes.service';
+import { Note } from '../classes/note';
 
 @Component({
   selector: 'app-note-create-edit',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteCreateEditComponent implements OnInit {
 
-  constructor() { }
+  private note: Note;
+
+  constructor(private noteService: NoteService,
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+
+    this.note = new Note();
+
+    this.route.paramMap.subscribe(
+      (params) => {
+        if (!params.get('id')) {
+          return;
+          // It create a note, there is no note id
+        }
+        const id = params.get('id');
+        this.getNoteData(id);
+      }
+    );
+  }
+
+  getNoteData(id) {
+    this.noteService.getNote(id)
+    .then((result) => {
+      console.log('ok: ', result);
+      const data = JSON.parse(result);
+      const note = new Note();
+      note.id = data['id'];
+      note.title = data['title'];
+      note.text = data['text'];
+      this.note = note;
+    })
+    .catch((err) => {
+     console.log('error: ', err.message);
+    });
+  }
+
+  saveNote() {
+  }
+
+  updateUser() {
+
+  }
+
+  createUser() {
+
   }
 
 }
