11c8fdd43eb9e4ad03f6cb22d5e6c416868ec1d0
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-06-18 19:31:26 +0200

    Initial work on signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,7 +7,7 @@ import { User } from '../classes/user';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
 
