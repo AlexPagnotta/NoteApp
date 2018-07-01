2bc54ebb6a2f96c43e580abab73477f253525510
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-07-01 16:26:21 +0200

    Changes to searchbar ui

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,28 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { User } from '../classes/user';
 import { Router } from '@angular/router';
 import { ViewChild } from '@angular/core';
 import { MdcMenu, MdcMenuItem } from '@angular-mdc/web';
 
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
+
 export class NavbarComponent implements OnInit {
 
   private user: User;
   private selectedIndex = -1;
+  private isSearchVisible = false;
+  private isMobile: boolean;
 
   constructor(private auth: AuthService,  private router: Router ) {
     this.user = auth.getUserData();
+    this.isMobile = window.innerWidth < 840;
   }
 
   @ViewChild('menu') menu: MdcMenu;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.isMobile = window.innerWidth < 840;
+  }
   ngOnInit() {
+  }
+
+  showSearchBar() {
+    this.isSearchVisible = true;
+  }
+
+  hideSearchBar() {
+    this.isSearchVisible = false;
   }
 
   handleMenuSelect(event: { index: number, item: MdcMenuItem }) {
