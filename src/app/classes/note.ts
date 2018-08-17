1620e12ac1d4860909c5fde245531fba0d5ec6dd
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-08-17 15:25:38 +0200

    Fix max card lenght

--- src/app/classes/note.ts
+++ src/app/classes/note.ts
@@ -1,12 +1,13 @@
-export class Note{
+export class Note {
 
     id: number;
     title: string;
     text: string;
 
-    constructor(){
+    constructor() {
         this.id = 0,
-        this.title = "",
-        this.text=""
+        this.title = '',
+        // tslint:disable-next-line:semicolon
+        this.text = ''
     }
 }
